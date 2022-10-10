4c107db0d8b4bacd23d8f7473b2ab7b2bd2e6221
tahar2004 <taharsoldaat@gmail.com>
2022-10-10 14:04:20 +0200

    product model klaar

--- app/Models/products.php
+++ app/Models/products.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class products extends Model
 {
     use HasFactory;
+    public function product_catogorie()
+    {
+        return  $this->belongsTo(ProductCatogorie::class);
+    }
+    public function invoice_products()
+    {
+        return $this->hasMany(InvoiceProducts::class,'product_id');
+    }
 }

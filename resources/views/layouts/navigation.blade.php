5a19009943c7995d11becd3d1e295d85866c822f
tahar2004 <taharsoldaat@gmail.com>
2022-11-07 11:01:59 +0100

    href toegevoegd
    nu hoef je niet in de url te typen om naar de hoofd site te gaan

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,7 +4,7 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <a href="" class="flex items-center">
+                <a href="https://barrocintens.dev/" class="flex items-center">
                     <img src="/fotos/logo/Logo5_groot.png" class="mr-3 h-6 sm:h-9" alt="Barrocintens logo">
                     <span class="self-center text-xl font-semibold whitespace-nowrap">Barrocintens</span>
                 </a>

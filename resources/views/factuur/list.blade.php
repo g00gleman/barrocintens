cc66950f8a87a0cb9851722b2af2193552c685c8
S.Klaren <Simon@klaren.nl>
2022-11-09 14:46:31 +0100

    fully functioning invoice

--- resources/views/factuur/list.blade.php
+++ resources/views/factuur/list.blade.php
@@ -24,7 +24,7 @@
                 <div class="md:flex-grow">
                 <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Factuur nr. {{ $AllInvoices->id }}</h2>
                 <p class="leading-relaxed">2x Koffie apparaat deluxe. 28x Koffie bonen super sterk. Bedrijf: {{ $AllInvoices->company_name }}.</p>
-                <a class="text-red-500 inline-flex items-center mt-4" href="/factuur/{{ $AllInvoices->id }}">Download Factuur
+                <a class="text-red-500 inline-flex items-center mt-4" href="/factuur/{{ $AllInvoices->id }} ">Download Factuur
                     <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M5 12h14"></path>
                     <path d="M12 5l7 7-7 7"></path>
